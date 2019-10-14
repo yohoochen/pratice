699ca967585823a89f1a7757b241875837260722
yohoochen <1435296366@qq.com>
2019-10-14 17:52:15 +0800

    update

--- kaggle.py
+++ kaggle.py
@@ -48,6 +48,75 @@ def log_rmse(net, features, labels):
         rmse = torch.sqrt(2 * loss(clipped_preds.log(), labels.log()).mean())
     return rmse.item()
 
-# def train(net, train_features,train_lables, test_features, test_lables, num_epochs, learning_rate, weight_decay, batch_sizes, loss):
-    # train_loss, test_loss =
+def train(net, train_features, train_labels, test_features, test_labels,
+          num_epochs, learning_rate, weight_decay, batch_size):
+    train_ls, test_ls = [], []
+    dataset = torch.utils.data.TensorDataset(train_features, train_labels)
+    train_iter = torch.utils.data.DataLoader(dataset, batch_size, shuffle=True)
+    # 这里使用了Adam优化算法
+    optimizer = torch.optim.Adam(params=net.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    net = net.float()
+    for epoch in range(num_epochs):
+        for X, y in train_iter:
+            l = loss(net(X.float()), y.float())
+            optimizer.zero_grad()
+            l.backward()
+            optimizer.step()
+        train_ls.append(log_rmse(net, train_features, train_labels))
+        if test_labels is not None:
+            test_ls.append(log_rmse(net, test_features, test_labels))
+    return train_ls, test_ls
+
+def get_k_fold_data(k, i, X, y):
+    # 返回第i折交叉验证时所需要的训练和验证数据
+    assert k > 1
+    fold_size = X.shape[0] // k
+    X_train, y_train = None, None
+    for j in range(k):
+        idx = slice(j * fold_size, (j + 1) * fold_size)
+        X_part, y_part = X[idx, :], y[idx]
+        if j == i:
+            X_valid, y_valid = X_part, y_part
+        elif X_train is None:
+            X_train, y_train = X_part, y_part
+        else:
+            X_train = torch.cat((X_train, X_part), dim=0)
+            y_train = torch.cat((y_train, y_part), dim=0)
+    return X_train, y_train, X_valid, y_valid
+
+def k_fold(k, X_train, y_train, num_epochs,
+           learning_rate, weight_decay, batch_size):
+    train_l_sum, valid_l_sum = 0, 0
+    for i in range(k):
+        data = get_k_fold_data(k, i, X_train, y_train)
+        net = get_net(X_train.shape[1])
+        train_ls, valid_ls = train(net, *data, num_epochs, learning_rate,
+                                   weight_decay, batch_size)
+        train_l_sum += train_ls[-1]
+        valid_l_sum += valid_ls[-1]
+        if i == 0:
+            d2l.semilogy(range(1, num_epochs + 1), train_ls, 'epochs', 'rmse',
+                         range(1, num_epochs + 1), valid_ls,
+                         ['train', 'valid'])
+        print('fold %d, train rmse %f, valid rmse %f' % (i, train_ls[-1], valid_ls[-1]))
+    return train_l_sum / k, valid_l_sum / k
+
+k, num_epochs, lr, weight_decay, batch_size = 5, 100, 5, 0, 64
+train_l, valid_l = k_fold(k, train_features, train_labels, num_epochs, lr, weight_decay, batch_size)
+print('%d-fold validation: avg train rmse %f, avg valid rmse %f' % (k, train_l, valid_l))
+
+def train_and_pred(train_features, test_features, train_labels, test_data,
+                   num_epochs, lr, weight_decay, batch_size):
+    net = get_net(train_features.shape[1])
+    train_ls, _ = train(net, train_features, train_labels, None, None,
+                        num_epochs, lr, weight_decay, batch_size)
+    d2l.semilogy(range(1, num_epochs + 1), train_ls, 'epochs', 'rmse')
+    print('train rmse %f' % train_ls[-1])
+    preds = net(test_features).detach().numpy()
+    test_data['SalePrice'] = pd.Series(preds.reshape(1, -1)[0])
+    submission = pd.concat([test_data['Id'], test_data['SalePrice']], axis=1)
+    submission.to_csv('./submission.csv', index=False)
+
+train_and_pred(train_features, test_features, train_labels, test_data, num_epochs, lr, weight_decay, batch_size)
+
 
