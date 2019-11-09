f4b8ee0b454937eaf1839b2e0897b2a71db8fe42
yohoochen <1435296366@qq.com>
2019-11-09 16:27:50 +0800

    update

--- utils/d2lzh.py
+++ utils/d2lzh.py
@@ -6,6 +6,7 @@ import numpy as np
 import torchvision
 import random
 import torch.nn as nn
+import time
 
 def use_svg_display():
     # 用矢量图显示
@@ -49,10 +50,10 @@ def load_data_fashion_mnist(batch_size, resize=None, root='./dataset'):
     transform = torchvision.transforms.Compose(trans)
     mnist_train = torchvision.datasets.FashionMNIST(root=root, train=True, download=True, transform=transform)
     mnist_test = torchvision.datasets.FashionMNIST(root=root, train=False, download=True, transform=transform)
-
+    # print(mnist_test)
     train_iter = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=4)
     test_iter = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=4)
-
+    # print(train_iter)
     return train_iter, test_iter
 #3.2
 def linreg(X, w, b):
@@ -79,20 +80,21 @@ def cross_entropy(y_hat, y):
     return - torch.log(y_hat.gather(1, y.view(-1, 1)))
 
 # 本函数已保存在d2lzh_pytorch
-def evaluate_accuracy(data_iter, net):
+def evaluate_accuracy(data_iter, net, device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')):
     acc_sum, n = 0.0, 0
-    for X, y in data_iter:
-        if isinstance(net, torch.nn.Module):
-            net.eval() # 评估模式, 这会关闭dropout
-            acc_sum += (net(X).argmax(dim=1) == y).float().sum().item()
-            net.train() # 改回训练模式
-        else: # 自定义的模型
-            if('is_training' in net.__code__.co_varnames): # 如果有is_training这个参数
-                # 将is_training设置成False
-                acc_sum += (net(X, is_training=False).argmax(dim=1) == y).float().sum().item()
-            else:
-                acc_sum += (net(X).argmax(dim=1) == y).float().sum().item()
-        n += y.shape[0]
+    with torch.no_grad():
+        for X, y in data_iter:
+            if isinstance(net, torch.nn.Module):
+                net.eval() # 评估模式, 这会关闭dropout
+                acc_sum += (net(X.to(device)).argmax(dim=1) == y.to(device)).float().sum().cpu().item()
+                net.train() # 改回训练模式
+            else: # 自定义的模型
+                if('is_training' in net.__code__.co_varnames): # 如果有is_training这个参数
+                    # 将is_training设置成False
+                    acc_sum += (net(X, is_training=False).argmax(dim=1) == y).float().sum().item()
+                else:
+                    acc_sum += (net(X).argmax(dim=1) == y).float().sum().item()
+            n += y.shape[0]
     return acc_sum / n
 
 
@@ -152,6 +154,30 @@ def corr2d(X, K):
         for j in range(Y.shape[1]):
             Y[i, j] = (X[i: i + h, j: j + w] * K).sum()
     return Y
+
+#5.5
+def train_ch5(net, train_iter, test_iter, batch_size, optimizer, device, num_epochs):
+    net = net.to(device)
+    print("training on ", device)
+    loss = torch.nn.CrossEntropyLoss()
+    batch_count = 0
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n, start = 0.0, 0.0, 0, time.time()
+        for X, y in train_iter:
+            X = X.to(device)
+            y = y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat, y)
+            optimizer.zero_grad()
+            l.backward()
+            optimizer.step()
+            train_l_sum += l.cpu().item()
+            train_acc_sum += (y_hat.argmax(dim=1) ==y).sum().cpu().item()
+            n += y.shape[0]
+            batch_count += 1
+        test_acc = evaluate_accuracy(test_iter, net)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f, time %.1f sec'
+              % (epoch + 1, train_l_sum / batch_count, train_acc_sum / n, test_acc, time.time() - start))
 
 
 if __name__ == "__main__":
